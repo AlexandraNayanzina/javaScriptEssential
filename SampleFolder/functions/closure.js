12e025b2e72c2729358fe5c292c5e389534170b3
AlexN <alexandranayanzina@gmail.com>
2024-12-07 14:43:47 -0500

    Closure exmaples

--- SampleFolder/functions/closure.js
+++ SampleFolder/functions/closure.js
@@ -1,11 +1,40 @@
-function outerFunction(){
-    const outerVar = "I am from the outer function";
+/* Closure is a function that remembers the variables from
+its outer scope even after that scope has finished executing.
+*/
+// Example 1
+// function outerFunction(){
+//     const outerVar = "I am from the outer function";
+//
+//     function innerFunction(){
+//         console.log(outerVar);
+//     }
+//     return innerFunction;
+// }
+//
+// const closure = outerFunction();
+// closure();
 
-    function innerFunction(){
-        console.log(outerVar);
-    }
-    return innerFunction;
+
+// Example 2
+let myName = "Alex";
+
+function printName(){
+    console.log(myName);
 }
 
-const closure = outerFunction();
-closure();
+// printName();
+myName = "NewName";
+// printName() // NewName
+
+
+// Example 3
+function outerFunc(outerVar){
+    return function innerFunc(innerVar){
+        console.log("The inner variable: " + innerVar + "\n");
+        console.log("The outer variable: " + outerVar);
+    };
+}
+
+
+const newFunc = outerFunc("outerVar");
+newFunc("innerVar")
